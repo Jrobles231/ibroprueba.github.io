4a6bf9b7e5a26df1298dc804ca343f88e4e06225
Jrobles231 <johan.robles.nilo24@gmail.com>
2025-07-25 01:06:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+        });
+
+const cambioColor = document.getElementById("boton_wsp");
+
+cambioColor.addEventListener("mouseover", function() {
+    cambioColor.style.backgroundColor = "#62adeb";
+
+});
+
+
+window.addEventListener("scroll", function() {
+    const nosotros = document.getElementById("nosotros_id");
+    const posicion = nosotros.getBoundingClientRect();
+
+    if (posicion.top < window.innerHeight - 100) {
+        nosotros.classList.add("visible");
+  } else {
+        nosotros.classList.remove("visible");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const preguntas = document.querySelectorAll(".pregunta h4");
+
+  preguntas.forEach(function (h4) {
+    h4.addEventListener("click", function () {
+      // Alternar clase "activo" solo en el h4 clickeado
+      h4.classList.toggle("activo");
+    });
+  });
+});
+
